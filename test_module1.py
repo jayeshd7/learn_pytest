8c921383e916b1a250e932427a8c06b343602da2
jdalal2 <jdalal2@slb.com>
2025-01-12 19:30:57 +0530

    [Jayesh] - checking marking and define pytest.ini

--- test_module1.py
+++ test_module1.py
@@ -1,16 +1,25 @@
+import pytest
+pytestmark = [pytest.mark.P0, pytest.mark.P1]
+@pytest.mark.P0
 def test_func1():
     a = 2
     b = 3
     assert a + b == 5
 
 
+@pytest.mark.P0
 def test_func2():
     a = 2
     b = 3
     assert a * b == 6
 
-
+@pytest.mark.P0
 def test_func3():
     a = 2
     b = 3
     assert a - b == -1
+
+@pytest.mark.P1
+def test_func4():
+   s1 = "python is a programming language"
+   assert s1.split() == ['python', 'is', 'a', 'programming', 'language']
